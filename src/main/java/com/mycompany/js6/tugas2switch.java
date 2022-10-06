24614a36d0324103d5ccdfaba7d50aa15e387cda
alixa <94752755+alixa01@users.noreply.github.com>
2022-10-06 19:10:57 +0700

    Update tugas2switch.java

--- src/main/java/com/mycompany/js6/tugas2switch.java
+++ src/main/java/com/mycompany/js6/tugas2switch.java
@@ -9,23 +9,43 @@ public class tugas2switch {
     public static void main (String args[]){
         int angka;
         
-        Scanner In = new Scanner(System.in);
+        Scanner dataIn = new Scanner(System.in);
         System.out.print("Masukkan Angka : ");
-        angka = In.nextInt();
+        angka = dataIn.nextInt();
         
         switch (angka) {
-            case 1 -> System.out.println("Satu");
-            case 2 -> System.out.println("Dua");
-            case 3 -> System.out.println("Tiga");
-            case 4 -> System.out.println("Empat");
-            case 5 -> System.out.println("Lima");
-            case 6 -> System.out.println("Enam");
-            case 7 -> System.out.println("Tujuh");
-            case 8 -> System.out.println("Delapan");
-            case 9 -> System.out.println("Sembilan");
-            case 10 -> System.out.println("Sepuluh");
-            default -> {
-            }
+            case 1:
+                System.out.println("Satu");
+                break;
+            case 2:
+                System.out.println("Dua");
+                break;
+            case 3:
+                System.out.println("Tiga");
+                break;
+            case 4:
+                System.out.println("Empat");
+                break;
+            case 5:
+                System.out.println("Lima");
+                break;
+            case 6:
+                System.out.println("Enam");
+                break;
+            case 7:
+                System.out.println("Tujuh");
+                break;
+            case 8:
+                System.out.println("Delapan");
+                break;
+            case 9:
+                System.out.println("Sembilan");
+                break;
+            case 10:
+                System.out.println("Sepuluh");
+                break;
+            default:
+                break;
         }
         
         
